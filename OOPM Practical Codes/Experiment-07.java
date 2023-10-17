93e70a82c598e4ad140106d229e9c3a6c2e24c02
Karan Singh <127474239+Karan-S1ngh@users.noreply.github.com>
2023-10-18 00:32:17 +0530

    Palindrome

--- OOPM Practical Codes/Experiment-07.java
+++ OOPM Practical Codes/Experiment-07.java
@@ -7,8 +7,7 @@ public class Main {
         System.out.println("Enter a string:");
         str = s.nextLine();
         StringBuffer str1 = new StringBuffer(str);
-        str1.reverse();
-        rev = str1.toString();
+        rev = str1.reverse().toString();
         if (str.equals(rev)) {            
             System.out.println("Yes, \"" + str + "\" is a palindrome.");
         } 
