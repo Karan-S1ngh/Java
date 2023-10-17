2c46098beec422997b6685c296ea07fc349ab679
Karan Singh <127474239+Karan-S1ngh@users.noreply.github.com>
2023-10-17 20:57:06 +0530

    Q4

---
+++ OOPM Rest Codes/IA 2 Practice/Q4.java
@@ -0,0 +1,62 @@
+/*
+Q4
+Through Custom exception user can raise application-specific error code. 
+Write a JAVA program to calculate a square of even number provided as input by user. 
+However, if a user provides an odd number as input, 
+then an exception must be thrown explicitly with message indicating the input number must be even number
+(Experiment 10)
+*/
+
+import java.util.Scanner;
+//Create custom exception OddException
+class OddException extends Exception {
+    int num;
+
+    // Constructor to get input number
+    public OddException(int num) {
+        num = num; // Assigning the local variable num to the instance variable num
+    }
+
+    // Override toString() method to display a message
+    @Override
+    public String toString() {
+        return "OddException: Input number must be even, but got odd number";
+    }
+}
+
+// Create a class MyExceptionDemo
+class MyExceptionDemo {
+    // Write a static function to throw OddException
+    public static void OddNoException(int num) throws OddException {
+        if (num % 2 != 0) {
+            throw new OddException(num);
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = s.nextInt();
+        try {
+            // Invoke OddNoException() in try block
+            MyExceptionDemo.OddNoException(x);
+            // Calculate and print square if the number is even
+            int square = x * x;
+            System.out.println("Square of the number: " + square);
+        } catch (OddException e) {
+            // Catch and print the custom exception message
+            System.out.println(e);
+        }
+    }
+}
+
+
+/*OUTPUT
+Enter a number: 5
+OddException: Input number must be even, but got odd number
+
+Enter a number: 12
+Square of the number: 144
+*/
