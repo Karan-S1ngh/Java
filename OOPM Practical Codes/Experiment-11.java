89d797d7d2c7ff2282ed83ac89022f7e50a45a1f
Karan Singh <127474239+Karan-S1ngh@users.noreply.github.com>
2023-10-29 21:12:31 +0530

    Multithreading

--- OOPM Practical Codes/Experiment-11.java
+++ OOPM Practical Codes/Experiment-11.java
@@ -1,19 +1,20 @@
 // Define a class implementing Runnable to print numbers
 class PrintNumbers implements Runnable {
     public void run() {
-        System.out.println("Printing Numbers:");
+        System.out.print("Printing Numbers: ");
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 }
 
 // Define another class implementing Runnable to print squares
 class PrintSquares implements Runnable {
     public void run() {
-        System.out.println("Printing Squares:");
+        System.out.print("Printing Squares: ");
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i * i);
+            System.out.print(i * i + " ");
         }
     }
 }
@@ -25,8 +26,17 @@ class MultiThread {
         Thread t1 = new Thread(new PrintNumbers());
         Thread t2 = new Thread(new PrintSquares());
 
-        // Start threads
+        // Start thread t1
         t1.start();
+
+        try {
+            // Wait for t1 to finish before starting t2
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Start thread t2 after t1 has finished
         t2.start();
 
         // Stop
@@ -35,27 +45,8 @@ class MultiThread {
 
 
 
+
 /*OUTPUT
-Printing Numbers:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-Printing Squares:
-1
-4
-9
-16
-25
-36
-49
-64
-81
-100
+Printing Numbers: 1 2 3 4 5 6 7 8 9 10 
+Printing Squares: 1 4 9 16 25 36 49 64 81 100 
 */
